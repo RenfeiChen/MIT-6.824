931a2b9f5b03f96d5291f1a154b02ac0ccd5aa9c
unknown <chenrf96@uw.edu>
2019-01-16 12:20:40 -0800

    MapReduce lab1 part3,4 done

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -1,6 +1,9 @@
 package mapreduce
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 //
 // schedule() starts and waits for all tasks in the given phase (mapPhase
@@ -30,5 +33,60 @@ func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, re
 	//
 	// Your code here (Part III, Part IV).
 	//
+
+	var wg sync.WaitGroup
+	wg.Add(ntasks)
+
+	// Add all task numbers to taskNumber channel and use waitgroup to wait
+	// all tasks to get completed, remmmember to close the channel or we cannot
+	// use the range since it wll get dead lock, the receiver doesn't know if
+	// it has ended the reason why we use a new goroutine is that we can make
+	// sure when we insert a task number into the channel there will be a
+	// receriver to receive this task concurrently
+
+	// ##Important
+	// We need to use wg.Wait() earlier than closing the channel
+	// Or we can use the wg.Wait() in the main function
+	// The reason is that if we put close ealier than wait, after receiving all
+	// taskNumber, the i loop in the main function will end, no matter if there
+	// is any running goroutine, the main function will end, so the gorouinte
+	// will get terminated, and the task cannot be done
+
+	taskNumberChan := make(chan int)
+	go func() {
+		for i := 0; i < ntasks; i++ {
+			taskNumberChan <- i
+		}
+		//wg.Wait()
+		close(taskNumberChan)
+	}()
+
+	// Schedule all tasks to works
+
+	for i := range taskNumberChan {
+		// declare the taskArgs
+		task := DoTaskArgs{JobName: jobName, TaskNumber: i, NumOtherPhase: n_other, Phase: phase}
+		if phase == mapPhase {
+			task.File = mapFiles[i]
+		}
+		// get the free worker
+		worker := <-registerChan
+		// schedule the task to the worker
+		go func(task DoTaskArgs, worker string) {
+			if call(worker, "Worker.DoTask", task, nil) {
+				// if the worker is available we should let the wg minus 1 and
+				// return the worker to the register channel
+				wg.Done()
+				registerChan <- worker
+			} else {
+				// the worker is not available we need to reschdule this task
+				// number so we need to return the number to the taskNumberChan
+				taskNumberChan <- task.TaskNumber
+			}
+		}(task, worker)
+	}
+
+	wg.Wait()
+
 	fmt.Printf("Schedule: %v done\n", phase)
 }
